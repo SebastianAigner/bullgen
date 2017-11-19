4e7e11c8f61facbd6329f03a99ad835db877bb9d
Sebastian Aigner <sebastian.aigner@tum.de>
2017-11-19 01:51:25 +0100

    factor our sentence part provider

--- src/main/kotlin/generators/CorporateSlideGenerator.kt
+++ src/main/kotlin/generators/CorporateSlideGenerator.kt
@@ -7,13 +7,10 @@ import providers.provideCorporateBulletpoint
 
 class CorporateSlideGenerator : SlideGenerator {
     override fun generate(keywords: Array<String>): HTMLString {
-
-        return "<h1>Main Corporate Strategy</h1>" +
-                "<ol>" +
-                "<li>${provideCorporateBulletpoint()}</li>" +
-                "<li>${provideCorporateBulletpoint()}</li>" +
-                "<li>${provideCorporateBulletpoint()}</li>" +
-                "<li>${provideCorporateBulletpoint()}</li>" +
-                "</ol>"
+        val listElements = Array(4) { provideCorporateBulletpoint() }.map { "<li>$it</li>" }.joinToString(" ")
+        return """<h1>Main Corporate Strategy</h1>
+            <ol>
+            $listElements
+            </ol>"""
     }
 }
